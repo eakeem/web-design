900905594f5fa663200970835795ed072c4ead5d
eakeem <eakeem0@gmail.com>
2026-02-15 18:41:39 -0500

    added bootstrap

--- java/site.js
+++ java/site.js
@@ -16,3 +16,15 @@
     $('#jquerybtn').click(function(){
         $('p').hide();
     })
+    $(
+        function(){
+            var pages=['index','about','contact']
+            var pathname=window.location.pathname;
+            $('.nav-link').each(function(item){
+                if(pathname.includes(pages[item])){
+                    $(this).addClass('active');
+                    $(this).attr('aria-current','page');
+                }
+            })
+        }
+    )
